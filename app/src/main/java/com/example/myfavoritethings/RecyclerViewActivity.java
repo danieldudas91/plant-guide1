7cea9fc025eaf163ff04129192861cc3060476a2
dd <duddaniel0919@gmail.com>
2023-04-26 13:16:16 +0200

    create and set new ListAdapter with list of words for RecyclerView

--- app/src/main/java/com/example/myfavoritethings/RecyclerViewActivity.java
+++ app/src/main/java/com/example/myfavoritethings/RecyclerViewActivity.java
@@ -2,12 +2,33 @@ package com.example.myfavoritethings;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.LinkedList;
 
 public class RecyclerViewActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ListAdapter listAdapter;
+    private LinkedList<String> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
+        list = createAndFillList();
+        recyclerView = findViewById(R.id.recycler_view);
+        listAdapter =new ListAdapter(this, list);
+        recyclerView.setAdapter(listAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    public LinkedList<String> createAndFillList(){
+        LinkedList<String> list = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add("Element" + i);
+        }
+        return list;
     }
 }
