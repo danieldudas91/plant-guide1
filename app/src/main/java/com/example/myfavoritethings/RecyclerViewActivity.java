599aa2647aead587e5f1ea8737281e910747519e
dd <duddaniel0919@gmail.com>
2023-04-27 21:55:49 +0200

    implement API data fetching

--- app/src/main/java/com/example/myfavoritethings/RecyclerViewActivity.java
+++ app/src/main/java/com/example/myfavoritethings/RecyclerViewActivity.java
@@ -5,6 +5,17 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.LinkedList;
 
 public class RecyclerViewActivity extends AppCompatActivity {
@@ -17,11 +28,16 @@ public class RecyclerViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
-        list = createAndFillList();
+        list = new LinkedList<>();
         recyclerView = findViewById(R.id.recycler_view);
-        listAdapter =new ListAdapter(this, list);
+        listAdapter = new ListAdapter(getApplicationContext(), list);
+        initRecyclerView(listAdapter);
+        fetchData();
+    }
+
+    private void initRecyclerView(ListAdapter listAdapter) {
         recyclerView.setAdapter(listAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
     }
 
     public LinkedList<String> createAndFillList(){
@@ -30,5 +46,39 @@ public class RecyclerViewActivity extends AppCompatActivity {
             list.addLast("Element" + i);
         }
         return list;
+    }
+
+    public void fetchData(){
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String url = "https://api.npoint.io/da5a5926e7caa3d1a8c9";
+
+        StringRequest request = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        parseJSON(response);
+                        listAdapter.setList(list);
+                        initRecyclerView(listAdapter);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        requestQueue.add(request);
+    }
+
+    private void parseJSON(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("users");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject user = jsonArray.getJSONObject(i);
+                list.addLast(user.getString("name") + " , " + user.getString("age"));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
