a4df5f4e225969de78888f462fce2496ea8ce8df
dd <duddaniel0919@gmail.com>
2023-06-13 19:01:39 +0200

    add @Entity, @PrimaryKey, @ColumnInfo annotations

--- app/src/main/java/com/example/myfavoritethings/model/Plant.java
+++ app/src/main/java/com/example/myfavoritethings/model/Plant.java
@@ -1,14 +1,22 @@
 package com.example.myfavoritethings.model;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "plants")
 public class Plant {
 
+    @PrimaryKey(autoGenerate = true)
     private int id;
     private String name;
+    @ColumnInfo(name = "scientific_name")
     private String scientificName;
+    @ColumnInfo(name = "picture_url")
     private String pictureUrl;
     private String description;
 
-    public Plant(String name, String scientificName, String scientificName1, String pictureUrl, String description) {
+    public Plant(String name, String scientificName1, String pictureUrl, String description) {
         this.name = name;
         this.scientificName = scientificName1;
         this.pictureUrl = pictureUrl;
