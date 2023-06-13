21b52143a06ac68413a34b301485f48cd2b717c9
dd <duddaniel0919@gmail.com>
2023-06-13 19:32:02 +0200

    implement CRUD methods

--- app/src/main/java/com/example/myfavoritethings/data/repository/PlantRepository.java
+++ app/src/main/java/com/example/myfavoritethings/data/repository/PlantRepository.java
@@ -1,6 +1,51 @@
 package com.example.myfavoritethings.data.repository;
 
+import android.app.Application;
+
+
+import androidx.lifecycle.LiveData;
+
+import com.example.myfavoritethings.data.DAO.PlantDao;
+import com.example.myfavoritethings.data.database.PlantDatabase;
+import com.example.myfavoritethings.model.Plant;
+import java.util.List;
+
 public class PlantRepository {
 
+    private PlantDao plantDao;
+    private LiveData<List<Plant>> allPlants;
 
+    public PlantRepository(Application application) {
+        PlantDatabase database = PlantDatabase.getInstance(application);
+        this.plantDao = database.plantDao();
+        this.allPlants = plantDao.getAllPlants();
+    }
+
+    public LiveData<List<Plant>> getAllPlants() {
+        return allPlants;
+    }
+
+    public void insert(Plant plant){
+        PlantDatabase.databaseWriteExecutor.execute(() -> {
+            plantDao.insert(plant);
+        });
+    }
+
+    public void deleteAll(){
+        PlantDatabase.databaseWriteExecutor.execute(() -> {
+            plantDao.deleteAll();
+        });
+    }
+
+    public void deleteById(int id){
+        PlantDatabase.databaseWriteExecutor.execute(() ->{
+            plantDao.deleteById(id);
+        });
+    }
+
+    public void updatePlant(String name, String scientificName, String description, String imageUrl, int id){
+        PlantDatabase.databaseWriteExecutor.execute(() ->{
+            plantDao.updatePlant(name, scientificName, description, imageUrl, id);
+        });
+    }
 }
