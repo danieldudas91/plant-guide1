7efd288de16f30747657398e63366d6910d4d69c
dd <duddaniel0919@gmail.com>
2023-06-13 18:40:54 +0200

    implement needed fields and constructor

--- app/src/main/java/com/example/myfavoritethings/model/Plant.java
+++ app/src/main/java/com/example/myfavoritethings/model/Plant.java
@@ -2,5 +2,16 @@ package com.example.myfavoritethings.model;
 
 public class Plant {
 
-    
+    private int id;
+    private String name;
+    private String scientificName;
+    private String pictureUrl;
+    private String description;
+
+    public Plant(String name, String scientificName, String scientificName1, String pictureUrl, String description) {
+        this.name = name;
+        this.scientificName = scientificName1;
+        this.pictureUrl = pictureUrl;
+        this.description = description;
+    }
 }
