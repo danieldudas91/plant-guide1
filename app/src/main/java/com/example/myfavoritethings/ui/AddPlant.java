fb45473e17f7411f4ff4f4040107b3c079cbb5ec
dd <duddaniel0919@gmail.com>
2023-06-14 06:17:00 +0200

    create AddPlant activity

---
+++ app/src/main/java/com/example/myfavoritethings/ui/AddPlant.java
@@ -0,0 +1,17 @@
+package com.example.myfavoritethings.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myfavoritethings.R;
+
+public class AddPlant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_plant);
+        System.out.println("ADDACTIVITY");
+    }
+}
