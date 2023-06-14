dc554a0bd83e64e84a86311aa4d80f79c576b1c4
dd <duddaniel0919@gmail.com>
2023-06-14 06:21:05 +0200

    create and implement PlantViewModel

---
+++ app/src/main/java/com/example/myfavoritethings/viewmodel/PlantViewModel.java
@@ -0,0 +1,42 @@
+package com.example.myfavoritethings.viewmodel;
+
+import android.app.Application;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import com.example.myfavoritethings.data.repository.PlantRepository;
+import com.example.myfavoritethings.model.Plant;
+import java.util.List;
+
+public class PlantViewModel extends AndroidViewModel {
+
+    private LiveData<List<Plant>> allPlants;
+    private PlantRepository plantRepository;
+
+    public PlantViewModel(@NonNull Application application) {
+        super(application);
+        plantRepository = new PlantRepository(application);
+        allPlants = plantRepository.getAllPlants();
+    }
+
+    public LiveData<List<Plant>> getAllPlants() {
+        return allPlants;
+    }
+
+    public void insert(Plant plant){
+        plantRepository.insert(plant);
+    }
+
+    public void deleteAll(){
+        plantRepository.deleteAll();
+    }
+
+    public void deleteById(int id){
+        plantRepository.deleteById(id);
+    }
+
+    public void updatePlant(String name, String scientificName, String description, String imageUrl, int id){
+        plantRepository.updatePlant(name, scientificName, description, imageUrl, id);
+    }
+}
+
