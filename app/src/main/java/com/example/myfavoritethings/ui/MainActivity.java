4d7f56d2628337a4fbc090901bf9ccb170ca885f
dd <duddaniel0919@gmail.com>
2023-06-14 06:19:24 +0200

    implement MainActivity

---
+++ app/src/main/java/com/example/myfavoritethings/ui/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.myfavoritethings.ui;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myfavoritethings.R;
+import com.example.myfavoritethings.viewmodel.PlantViewModel;
+import com.example.tripplanner.utils.PlantListAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private PlantViewModel viewModel;
+    private RecyclerView recyclerView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerview);
+        PlantListAdapter adapter = new PlantListAdapter(new PlantListAdapter.PlantDiff());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        viewModel = new ViewModelProvider(this)
+                .get(PlantViewModel.class);
+        viewModel.getAllPlants()
+                .observe(this, adapter::submitList);
+    }
+}
