719f6223e82450d8aaadce43ca11e0120cff45fb
dd <duddaniel0919@gmail.com>
2023-06-20 11:00:33 +0200

    create GeographicRegion enum class with 6 main hungarian geographical regions

---
+++ app/src/main/java/com/example/myfavoritethings/model/GeographicRegion.java
@@ -0,0 +1,10 @@
+package com.example.myfavoritethings.model;
+
+public enum GeographicRegion {
+    GREAT_PLAIN,
+    TRANS_DANUBIAN_RANGE,
+    NORTH_HUNGARIAN_MOUNTAINS,
+    TRANS_DANUBIAN_HILLS,
+    LITTLE_ALFÖLD,
+    WESTERN_CONFINE
+}
