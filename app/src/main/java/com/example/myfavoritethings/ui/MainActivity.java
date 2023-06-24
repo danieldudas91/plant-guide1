3ecfebaaad10edfd4abf8485b1200bb4c60e2922
dd <duddaniel0919@gmail.com>
2023-06-24 21:04:36 +0200

    implement OnItemSelectedListener for dropdown list

--- app/src/main/java/com/example/myfavoritethings/ui/MainActivity.java
+++ app/src/main/java/com/example/myfavoritethings/ui/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -19,7 +20,7 @@ import com.example.myfavoritethings.model.GeographicRegion;
 import com.example.myfavoritethings.viewmodel.PlantViewModel;
 import com.example.tripplanner.utils.PlantListAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private PlantViewModel viewModel;
     private RecyclerView recyclerView;
@@ -36,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         sortByDropdown = findViewById(R.id.sort_by_region);
         setRegionSpinnerElements(this, sortByDropdown);
         item = new MutableLiveData<>();
+        sortByDropdown.setOnItemSelectedListener(this);
         PlantListAdapter adapter = new PlantListAdapter(new PlantListAdapter.PlantDiff());
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -57,5 +59,15 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View view){
         Intent intent = new Intent(this, AddPlant.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        item.setValue((GeographicRegion) adapterView.getSelectedItem());
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        System.out.println("nothing selected");
     }
 }
