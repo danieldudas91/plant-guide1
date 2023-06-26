c5429d577b293c3aefd454538a969f6dcdf3282d
danieldudas91 <duddaniel0919@gmail.com>
2023-06-26 19:40:12 +0200

    add label of String type to values

--- app/src/main/java/com/example/myfavoritethings/model/GeographicRegion.java
+++ app/src/main/java/com/example/myfavoritethings/model/GeographicRegion.java
@@ -1,10 +1,17 @@
 package com.example.myfavoritethings.model;
 
 public enum GeographicRegion {
-    GREAT_PLAIN,
-    TRANS_DANUBIAN_RANGE,
-    NORTH_HUNGARIAN_MOUNTAINS,
-    TRANS_DANUBIAN_HILLS,
-    LITTLE_ALFÖLD,
-    WESTERN_CONFINE
+    GREAT_PLAIN("Great Plain"),
+    TRANS_DANUBIAN_RANGE("Transdanubian Range"),
+    NORTH_HUNGARIAN_MOUNTAINS("North Hungarian Mountains"),
+    TRANS_DANUBIAN_HILLS("Transdanubian Hills"),
+    LITTLE_ALFÖLD("Little Alföld"),
+    WESTERN_CONFINE("Western Confine");
+    private String label;
+
+    GeographicRegion(String label) {
+        this.label = label;
+    }
+
+    
 }
