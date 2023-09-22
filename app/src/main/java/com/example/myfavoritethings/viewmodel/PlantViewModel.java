07f762f61af5d7163315ea7df8a031c8de691fae
dd <duddaniel0919@gmail.com>
2023-09-22 15:51:26 +0200

    implement caching

--- app/src/main/java/com/example/myfavoritethings/viewmodel/PlantViewModel.java
+++ app/src/main/java/com/example/myfavoritethings/viewmodel/PlantViewModel.java
@@ -4,9 +4,12 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import com.example.myfavoritethings.data.cache.PlantCache;
 import com.example.myfavoritethings.data.repository.PlantRepository;
 import com.example.myfavoritethings.model.GeographicRegion;
 import com.example.myfavoritethings.model.Plant;
+import java.util.HashMap;
 import java.util.List;
 
 public class PlantViewModel extends AndroidViewModel {
@@ -25,22 +28,29 @@ public class PlantViewModel extends AndroidViewModel {
     }
 
     public LiveData<List<Plant>> getPlantsByRegion(GeographicRegion region) {
-        return plantRepository.getPlantsByRegion(region);
+        HashMap<GeographicRegion, List<Plant>> cache = PlantCache.getINSTANCE().cache;
+        List<Plant> plantsInCache = cache.get(region);
+        if(plantsInCache != null){
+            return (LiveData<List<Plant>>) new MutableLiveData<>(plantsInCache);
+        }
+        LiveData<List<Plant>> result = plantRepository.getPlantsByRegion(region);
+        cache.put(region, result.getValue());
+        return result;
     }
 
-    public void insert(Plant plant){
+    public void insert(Plant plant) {
         plantRepository.insert(plant);
     }
 
-    public void deleteAll(){
+    public void deleteAll() {
         plantRepository.deleteAll();
     }
 
-    public void deleteById(int id){
+    public void deleteById(int id) {
         plantRepository.deleteById(id);
     }
 
-    public void updatePlant(String name, String scientificName, String description, String imageUrl, int id){
+    public void updatePlant(String name, String scientificName, String description, String imageUrl, int id) {
         plantRepository.updatePlant(name, scientificName, description, imageUrl, id);
     }
 }
